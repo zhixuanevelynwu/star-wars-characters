d2187fbbe6598e68b1c53a63b530d60148ba5ad2
zhixuanevelynwu <zw1887@nyu.edu>
2024-02-02 18:28:25 -0500

    added basic pagination

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,38 +1,49 @@
-import React, { useEffect, useState } from "react";
+import useFetchCharacters from "./hooks/UseFetchCharacters";
+import CharacterCard from "./components/CharacterCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
-import CharacterCard from "./components/CharacterCard";
 
-/**
- * https://swapi.dev/
- * @returns display a list of all Star Wars characters using the endpoint “/people".
- */
-export default function MyApp() {
-  const [characters, setCharacters] = useState([]);
+const MyApp: React.FC = () => {
+  const {
+    characters,
+    loading,
+    error,
+    currentPage,
+    totalPages,
+    setCurrentPage,
+  } = useFetchCharacters();
 
-  useEffect(() => {
-    // Function to fetch characters
-    const fetchCharacters = async () => {
-      try {
-        const response = await fetch("https://swapi.dev/api/people/");
-        const data = await response.json();
-        console.log(data);
-        setCharacters(data.results);
-      } catch (error) {
-        console.error("Failed to fetch characters:", error);
-      }
-    };
-    fetchCharacters();
-  }, []);
+  const handlePreviousClick = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <div>
       <h1>Star Wars Characters</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
       <div id="card-container">
         {characters.map((character, index) => (
           <CharacterCard key={index} character={character} />
         ))}
       </div>
+      <div className="pagination">
+        <button onClick={handlePreviousClick} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNextClick} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default MyApp;
