30b1d59b53b4b12a19b1c1f41390eb5da9fc910b
zhixuanevelynwu <zw1887@nyu.edu>
2024-02-02 14:48:32 -0500

    updated character card display

--- my-app/src/components/CharacterCard.tsx
+++ my-app/src/components/CharacterCard.tsx
@@ -1,17 +1,34 @@
-import { Character } from '../models/Character';
+import { Character } from "../models/Character";
+import "../styles/CharacterCard.css";
 
 /**
- * 
+ *
  * @param param0 character information
- * @returns display a card with the name of each character along with a random picture 
- * for each character (see Picsum photos for random picture inspiration). Each character 
- * card should be colored based on their species and have some kind of animation when the 
- * user hovers over the card. When we click on a character’s card, more information should 
+ * @returns display a card with the name of each character along with a random picture
+ * for each character (see Picsum photos for random picture inspiration). Each character
+ * card should be colored based on their species and have some kind of animation when the
+ * user hovers over the card. When we click on a character’s card, more information should
  * appear in a modal about the character.
  */
 function CharacterCard({ character }: { character: Character }) {
+  // get a random background image
   return (
-    <button>{character.name}</button>
+    <div
+      className="card"
+      style={{
+        backgroundImage: `url(https://picsum.photos/seed/${
+          Math.random() * 1000
+        }/500/500)`,
+      }}
+    >
+      <div className="card-content">
+        <h2 className="card-title">{character.name}</h2>
+        <p className="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
+          culpa.
+        </p>
+      </div>
+    </div>
   );
 }
 
