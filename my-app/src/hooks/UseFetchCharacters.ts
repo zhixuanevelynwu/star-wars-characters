2d74945aaa91824c1f66246ebbfc04ecb26cc979
zhixuanevelynwu <zw1887@nyu.edu>
2024-02-02 18:27:05 -0500

    added custom hooks

---
+++ my-app/src/hooks/UseFetchCharacters.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect, useCallback } from 'react';
+import { Character } from '../models/Character';
+
+type FetchCharactersResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Character[];
+};
+
+const useFetchCharacters = (initialPage = 1) => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(initialPage);
+  const [totalPages, setTotalPages] = useState<number>(0);
+
+  const fetchCharacters = useCallback(async (page: number) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://swapi.dev/api/people/?page=${page}`);
+      const data: FetchCharactersResponse = await response.json();
+      setCharacters(data.results);
+      setError(null);
+
+      // Assuming 10 characters per page as per SWAPI documentation
+      const calculatedTotalPages = Math.ceil(data.count / 10);
+      setTotalPages(calculatedTotalPages);
+    } catch (error) {
+      setError("Failed to fetch characters");
+      console.error("Failed to fetch characters:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchCharacters(currentPage);
+  }, [currentPage, fetchCharacters]);
+
+  return { characters, loading, error, currentPage, totalPages, setCurrentPage };
+};
+
+export default useFetchCharacters;
---
+++ my-app/src/hooks/UseFetchPlanet.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Planet } from '../models/Planet';
+
+/**
+ * 
+ * @param homeworldUrl link to character's homeworld
+ * @returns 
+ */
+const useFetchPlanet = (homeworldUrl: string) => {
+  const [planet, setPlanet] = useState<Planet>({} as Planet);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPlanet = async () => {
+      if (!homeworldUrl) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await fetch(homeworldUrl);
+        const data = await response.json();
+        setPlanet(data);
+        setError(null);
+      } catch (error) {
+        setError("Failed to fetch planet");
+        console.error("Failed to fetch planet:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPlanet();
+  }, [homeworldUrl]);
+
+  return { planet, loading, error };
+};
+
+export default useFetchPlanet;
